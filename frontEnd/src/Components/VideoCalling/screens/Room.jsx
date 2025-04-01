15133b54833636a116dbb4fbbd4b5614d1fe71b6
Pratik Kashyap <kashyappratik2002@gmail.com>
2025-04-01 16:49:17 +0530

    Change in Room.jsx

--- frontEnd/src/Components/VideoCalling/screens/Room.jsx
+++ frontEnd/src/Components/VideoCalling/screens/Room.jsx
@@ -164,7 +164,7 @@ const Room = () => {
         // );
 
         if (peerInstance.current.webRTCPeer.iceConnectionState === "failed") {
-          toast.error("Connection Failed. Try Clicking Next!");
+          toast.error("Connection Failed. Click Next!");
         }
       };
 
@@ -227,7 +227,7 @@ const Room = () => {
           // );
 
           if (peerInstance.current.webRTCPeer.iceConnectionState === "failed") {
-            toast.error("Connection Failed. Try Clicking Next!");
+            toast.error("Connection Failed. Click Next!");
           }
         };
 
@@ -249,7 +249,7 @@ const Room = () => {
         const answer = await peerInstance.current.getAnswer(offer);
         socket.emit("answer", { answer, to: from });
       } catch (error) {
-        toast.error("Connection Error. Try Clicking Next!");
+        toast.error("Connection Failed. Click Next!");
       }
     });
 
@@ -264,7 +264,7 @@ const Room = () => {
           await peerInstance.current.setRemoteDescription(answer);
         } catch (error) {
           // console.error("Error setting remote answer:", error);
-          toast.error("Connection Error. Try Clicking Next!");
+          toast.error("Connection Failed. Click Next!");
         }
       } else {
         console.error(
@@ -358,7 +358,7 @@ const Room = () => {
     setMessageArray([]);
     socket.emit("next", { roomId, otherUserID });
     setRoomId(null);
-    toast.success("Finding Next User.");
+    toast.success("Finding Next User!");
   };
 
   // Handle "Stop" button click
