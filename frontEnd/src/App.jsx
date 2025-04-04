54be49e89deafd0a2524fe318933e584299bed60
Pratik Kashyap <135964490+ipratiik@users.noreply.github.com>
2025-04-04 17:28:18 +0530

    Update App.jsx
    Update App.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SocketProvider from "./Components/VideoCalling/context/SocketProvider";
 import Room from "./Components/VideoCalling/screens/Room";
 import Home from "./Components/Home/Home";
+import Maintenance from '../Maintenance';
+
+const isUnderMaintenance = true;
+
 function App() {
   return (
-    <>
-      <SocketProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/nitExclusive" element={<Room />} />
-          </Routes>
-        </BrowserRouter>
-      </SocketProvider>
-    </>
+    <SocketProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={isUnderMaintenance ? <Maintenance /> : <Home />}
+          />
+          <Route path="/nitExclusive" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketProvider>
   );
 }
 
