dd7c95d4038f681ad17ebea0e8cc62da5e710b14
Ankit <ankitchauhan21500@gmail.com>
2025-04-04 17:31:21 +0530

    site under maintainence

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SocketProvider from "./Components/VideoCalling/context/SocketProvider";
 import Room from "./Components/VideoCalling/screens/Room";
 import Home from "./Components/Home/Home";
-import Maintenance from '../Maintenance';
+import Maintenance from './Maintenance';
 
 const isUnderMaintenance = true;
 
