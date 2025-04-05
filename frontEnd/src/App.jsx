dedeee8fba025024a5c520e3a476a242895a33c2
Ankit <ankitchauhan21500@gmail.com>
2025-04-05 12:40:59 +0530

    maintainence

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -4,7 +4,7 @@ import Room from "./Components/VideoCalling/screens/Room";
 import Home from "./Components/Home/Home";
 import Maintenance from './Maintenance';
 
-const isUnderMaintenance = false;
+const isUnderMaintenance = true;
 
 function App() {
   return (
